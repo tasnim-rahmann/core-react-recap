9313da4df027c92ea7ef019463815dff21de9a24
tasnim-rahman <tasnimrahman0101@gmail.com>
2026-08-02 20:56:29 +0600

    bpl-projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { Suspense } from "react";
-import Countries from "./components/countries/Countries";
-import Loading from "./components/Loading";
-
-const countriesPromise = fetch('https://openapi.programming-hero.com/api/all').then(res => res.json());
+// const countriesPromise = fetch('https://openapi.programming-hero.com/api/all').then(res => res.json());
 
 function App() {
   return (
-    <div className="max-w-7xl mx-auto">
-      <h1 className="text-2xl font-semibold mt-20">Here are the countries!</h1>
-      {/* <Countries /> */}
+    <>Hello React!</>
 
-      <Suspense fallback={<><Loading /></>}>
-        <Countries countriesPromise={countriesPromise} />
-      </Suspense>
-    </div>
+    // <div className="max-w-7xl mx-auto">
+    //   {/* <h1 className="text-2xl font-semibold mt-20">Here are the countries!</h1> */}
+    //   {/* <Countries /> */}
+    //   {/*
+    //   <Suspense fallback={<><Loading /></>}>
+    //     <Countries countriesPromise={countriesPromise} />
+    //   </Suspense> */}
+    // </div>
   );
 }
 
