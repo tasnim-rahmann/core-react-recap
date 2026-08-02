7a560d5052380d786de9704feb1433a35e156934
tasnim-rahman <tasnimrahman0101@gmail.com>
2026-08-02 23:12:19 +0600

    design navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 // const countriesPromise = fetch('https://openapi.programming-hero.com/api/all').then(res => res.json());
 
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <>Hello React!</>
+    <>
+      <Navbar />
+    </>
 
     // <div className="max-w-7xl mx-auto">
     //   {/* <h1 className="text-2xl font-semibold mt-20">Here are the countries!</h1> */}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { TbCoinTakaFilled } from "react-icons/tb";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center max-w-7xl mx-auto my-8">
+      <div>
+        <img src="/public/logo.png" alt="" />
+      </div>
+      <div>
+        <ul className="flex items-center gap-10 uppercase text-sm">
+          <li className="hover:text-black/60 transition-colors duration-200">
+            <a href="">Home</a>
+          </li>
+          <li className="hover:text-black/60 transition-colors duration-200">
+            <a href="">Fixture</a>
+          </li>
+          <li className="hover:text-black/60 transition-colors duration-200">
+            <a href="">Teams</a>
+          </li>
+          <li className="hover:text-black/60 transition-colors duration-200">
+            <a href="">Schedules</a>
+          </li>
+          <li className="flex items-center gap-2 border px-4 py-2 rounded-sm shadow-md">
+            0 <TbCoinTakaFilled size={20} color="gold" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
